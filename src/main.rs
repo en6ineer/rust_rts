e91fe67deaf00b7b59c0bc17fd5bcfd482adf4bb
Danil <danil8kotenko@gmail.com>
2025-12-16 21:01:06 +0500

    add movement and sprite

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,39 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, hello_world)
+        .add_systems(Startup, setup)
+        .add_systems(Update, move_units)
         .run();
 }
 
-fn hello_world() {
-    println!("Hello from Bevy!");
+#[derive(Component)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Sprite {
+            color: Color::srgb(0.3, 0.8, 0.3),
+            custom_size: Some(Vec2::new(50.0, 50.0)),
+            ..default()
+        },
+        Transform::default(),
+        GlobalTransform::default(),
+        Velocity { x: 100.0, y: 10.0 },
+    ));
+}
+
+
+fn move_units(
+    time: Res<Time>,
+    mut query: Query<(&Velocity, &mut Transform)>,
+) {
+    for (velocity, mut transform) in &mut query {
+        transform.translation.x += velocity.x * time.delta_secs();
+        transform.translation.y += velocity.y * time.delta_secs();
+    }
 }
